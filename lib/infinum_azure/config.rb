bdeafa65cab60d034cee0ae69edcaec862ea45ff
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-05-24 13:43:57 +0200

    Remove defaults module

--- lib/infinum_azure/config.rb
+++ lib/infinum_azure/config.rb
@@ -2,12 +2,5 @@
 
 module InfinumAzure
   class Config
-    Defaults.all_attributes.each do |attr, value|
-      attr_writer attr
-
-      define_method(attr) do
-        instance_variable_set(:"@#{attr}", instance_variable_get(:"@#{attr}") || value)
-      end
-    end
   end
 end
