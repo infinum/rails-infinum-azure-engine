d236768c21f8461cb65fb911474d9dac5cab2866
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-06-04 17:50:32 +0200

    Delegate users_auth_url to config

--- lib/tasks/infinum_azure/users/request.rb
+++ lib/tasks/infinum_azure/users/request.rb
@@ -7,7 +7,7 @@ require_relative 'response'
 module InfinumAzure
   module Users
     class Request
-      URL = InfinumAzure.users_auth_url
+      URL = InfinumAzure.config.users_auth_url
 
       def self.execute
         raise 'infinum_azure_users_auth_url secret required for this rake task' if URL.blank?
